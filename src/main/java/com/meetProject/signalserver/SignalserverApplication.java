e52957455ab0bd0bea77263c0be78b2c56db2d9f
armd482 <armd482@naver.com>
2025-11-09 12:16:18 +0900

    init: 스프링 부트 기본 세팅

---
+++ src/main/java/com/meetProject/signalserver/SignalserverApplication.java
@@ -0,0 +1,13 @@
+package com.meetProject.signalserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignalserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignalserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetProject/signalserver/SignalserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetProject.signalserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignalserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
