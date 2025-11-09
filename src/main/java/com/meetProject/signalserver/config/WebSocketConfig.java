97bea0b413e73f4ffe5ee88e8256ba7872ba9c9e
armd482 <armd482@naver.com>
2025-11-09 16:55:51 +0900

    feat: uuid 번환 구현

--- src/main/java/com/meetProject/signalserver/config/WebSocketConfig.java
+++ src/main/java/com/meetProject/signalserver/config/WebSocketConfig.java
@@ -1,24 +1,33 @@
 package com.meetProject.signalserver.config;
 
+import java.security.Principal;
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+        registry.addEndpoint("/ws")
+                .setHandshakeHandler(new ChatHandshakeHandler())
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topic");
+        registry.enableSimpleBroker("/topic", "/queue");
         registry.setApplicationDestinationPrefixes("/app");
-        registry.setUserDestinationPrefix("/user");
+        registry.setUserDestinationPrefix("/user"); // 1:1 prefix
     }
-
 }
--- src/main/java/com/meetProject/signalserver/controller/SignalController.java
+++ src/main/java/com/meetProject/signalserver/controller/SignalController.java
@@ -3,26 +3,42 @@ package com.meetProject.signalserver.controller;
 import com.meetProject.signalserver.model.JoinPayload;
 import com.meetProject.signalserver.model.User;
 import com.meetProject.signalserver.service.RoomsManagementService;
+import java.security.Principal;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SignalController {
     private final RoomsManagementService roomsManagementService;
-    private final SimpMessagingTemplate simpMessagingTemplate;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public SignalController(RoomsManagementService roomsManagementService, SimpMessagingTemplate messagingTemplate) {
         this.roomsManagementService = roomsManagementService;
-        this.simpMessagingTemplate = messagingTemplate;
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    @MessageMapping("/register")
+    public void register(SimpMessageHeaderAccessor headers) {
+        Principal principal = headers.getUser();
+        if (principal == null) {
+            throw new RuntimeException("Principal is null");
+        }
+
+        String userId = principal.getName();
+        messagingTemplate.convertAndSendToUser(userId, "/queue/userId", Map.of("userId", userId));
     }
 
     @MessageMapping("/signal/join")
     public void join(@Payload JoinPayload joinPayload) {
         String targetRoomId = joinPayload.roomId();
         List<User> participants = roomsManagementService.getParticipants(targetRoomId);
-        simpMessagingTemplate.convertAndSendToUser(joinPayload.userId(), "queue/signal/join", participants);
+        messagingTemplate.convertAndSendToUser(joinPayload.userId(), "/topic/signal/join", participants);
     }
 }
