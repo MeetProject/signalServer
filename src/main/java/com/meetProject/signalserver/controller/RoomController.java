01041854f7dce084f2b9262d600ae59b6901c22b
armd482 <armd482@naver.com>
2025-11-14 16:41:30 +0900

    refactor: 방 생성, 강제 종료 controller 통합

--- src/main/java/com/meetProject/signalserver/controller/RoomController.java
+++ src/main/java/com/meetProject/signalserver/controller/RoomController.java
@@ -1,29 +1,40 @@
 package com.meetProject.signalserver.controller;
 
+import com.meetProject.signalserver.constant.SignalType;
 import com.meetProject.signalserver.constant.StreamType;
+import com.meetProject.signalserver.model.Room;
+import com.meetProject.signalserver.model.dto.CreateRoomResponse;
 import com.meetProject.signalserver.service.RoomsManagementService;
 import com.meetProject.signalserver.service.ScreenSharingService;
 import com.meetProject.signalserver.service.SignalMessagingService;
+import java.util.ArrayList;
+import java.util.UUID;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ForceLeaveController {
+public class RoomController {
     private final RoomsManagementService roomsManagementService;
     private final ScreenSharingService screenSharingService;
     private final SignalMessagingService signalMessagingService;
 
-    public ForceLeaveController(RoomsManagementService roomsManagementService, ScreenSharingService screenSharingService, SignalMessagingService signalMessagingService) {
+    public RoomController(RoomsManagementService roomsManagementService, ScreenSharingService screenSharingService, SignalMessagingService signalMessagingService) {
         this.roomsManagementService = roomsManagementService;
         this.screenSharingService = screenSharingService;
         this.signalMessagingService = signalMessagingService;
     }
 
+    @PostMapping("/create")
+    public CreateRoomResponse createRoom() {
+        String roomId = UUID.randomUUID().toString();
+        Room room = new Room(roomId, new ArrayList<>());
+        roomsManagementService.createRoom(room);
+        return new CreateRoomResponse(SignalType.CREATE,roomId);
+    }
+
     @PostMapping("/api/leave")
     public void forceLeave(@RequestParam String userId, @RequestParam String roomId) {
-        System.out.println("forceLeave");
-
         if(screenSharingService.isScreenSharingId(userId)) {
             screenSharingService.stopSharing(roomId);
             signalMessagingService.sendLeave(roomId, userId, StreamType.SCREEN);
