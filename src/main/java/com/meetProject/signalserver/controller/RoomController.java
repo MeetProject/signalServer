b76b38ed6ec24912c2770195d5c5a7b2f1859641
armd482 <armd482@naver.com>
2025-11-17 20:38:19 +0900

    feat: 방 id 유효성 검증 api 구현

--- src/main/java/com/meetProject/signalserver/controller/RoomController.java
+++ src/main/java/com/meetProject/signalserver/controller/RoomController.java
@@ -3,10 +3,12 @@ package com.meetProject.signalserver.controller;
 import com.meetProject.signalserver.constant.RoomRule;
 import com.meetProject.signalserver.model.Room;
 import com.meetProject.signalserver.model.dto.CreateRoomResponse;
+import com.meetProject.signalserver.model.dto.ValidateRoomResponse;
 import com.meetProject.signalserver.orchestration.LeaveOrchestrationService;
 import com.meetProject.signalserver.service.RoomsService;
 import com.meetProject.signalserver.util.RandomIdGenerator;
 import java.util.Collections;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,12 @@ public class RoomController {
         Room room = new Room(roomId, Collections.emptyList());
         roomsService.createRoom(room);
         return new CreateRoomResponse(roomId);
+    }
+
+    @GetMapping("/api/room/validate")
+    public ValidateRoomResponse validateRoom(@RequestParam String roomId) {
+        boolean isValidRoom = roomsService.exists(roomId) && roomsService.getParticipants(roomId).size() < RoomRule.MAX_ROOM_PARTICIPANTS;
+        return new ValidateRoomResponse(isValidRoom);
     }
 
     @PostMapping("/api/leave")
