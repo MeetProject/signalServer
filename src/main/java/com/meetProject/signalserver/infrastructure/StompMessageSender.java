466f58aa9c13f1ca1722f047aee522f7124aa5a5
armd482 <armd482@naver.com>
2026-07-01 12:32:18 +0900

    feat: STOMP 메시지 전송 게이트웨이 추가

---
+++ src/main/java/com/meetProject/signalserver/infrastructure/StompMessageSender.java
@@ -0,0 +1,32 @@
+package com.meetProject.signalserver.infrastructure;
+
+import com.meetProject.signalserver.constant.MediaType;
+import com.meetProject.signalserver.constant.TopicType;
+import com.meetProject.signalserver.dto.common.MediaPayload;
+import com.meetProject.signalserver.dto.common.SignalResponse;
+import com.meetProject.signalserver.dto.common.TopicResponse;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StompMessageSender {
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public StompMessageSender(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+
+    public void sendToUser(String userId, SignalResponse payload) {
+        messagingTemplate.convertAndSendToUser(userId, "/queue/replies", payload);
+    }
+
+    public void broadcast(String roomId, TopicType type, TopicResponse payload) {
+        String path = String.join("/",type.name().toLowerCase().split("_"));
+        messagingTemplate.convertAndSend("/topic/room/" + roomId + "/" + path, payload);
+    }
+
+    public void sendToMediaServer(MediaType type, MediaPayload payload) {
+        String path = String.join("/",type.name().toLowerCase().split("_"));
+        messagingTemplate.convertAndSendToUser("mediaServer","/media/" + path, payload);
+    }
+}
