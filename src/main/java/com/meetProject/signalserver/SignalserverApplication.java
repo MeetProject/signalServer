b3bd4b60575208557c659298d8a1f607ddc3cf58
armd482 <armd482@naver.com>
2026-07-01 12:32:18 +0900

    chore: JDBC/Spring Retry/AOP 의존성 및 설정 추가

--- src/main/java/com/meetProject/signalserver/SignalserverApplication.java
+++ src/main/java/com/meetProject/signalserver/SignalserverApplication.java
@@ -2,7 +2,9 @@ package com.meetProject.signalserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
 @SpringBootApplication
 public class SignalserverApplication {
 
