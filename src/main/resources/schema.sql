9344014b24cb222ecf047f455db155fc8b0f4c8b
armd482 <armd482@naver.com>
2026-07-03 18:48:23 +0900

    fix: 테스트 컨텍스트 중복 실행에 대비해 schema.sql 멱등화

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE member (
+CREATE TABLE IF NOT EXISTS member (
     id       VARCHAR(36)  PRIMARY KEY,
     name     VARCHAR(20)  NOT NULL,
     profile_color VARCHAR(20),
     created_at    TIMESTAMP    NOT NULL
 );
 
-CREATE TABLE room (
+CREATE TABLE IF NOT EXISTS room (
     id     VARCHAR(10) PRIMARY KEY,
     created_at  TIMESTAMP   NOT NULL
 );
